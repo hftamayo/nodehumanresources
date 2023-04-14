4388041abea2f1d8ae8970d55d81b13edba9a086
hftamayo <hftamayo@gmail.com>
2023-04-14 11:03:07 -0600

    adjusting express import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import "./database/connectdb.js";
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
