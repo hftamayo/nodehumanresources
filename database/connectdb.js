4f22eb5edff76c8fb89f23be221d964dacfed6d1
hftamayo <hftamayo@gmail.com>
2023-04-14 11:01:13 -0600

    verify connection to mongodb using mongoose

---
+++ database/connectdb.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+try {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connection established with Data Repository");
+} catch (error) {
+    console.log("Couldn't established connection with Data Repository, details: "+error);
+}
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require('express');
-const app = express()
+import "dotenv/config";
+import "./database/connectdb.js";
+const express = require("express");
+const app = express();
 
-app.listen(5000, () => console.log());
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log());
