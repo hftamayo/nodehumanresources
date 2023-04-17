1b8d2503d40cc83d1f2b6aa7a57dfc4ce7a4847f
hftamayo <hftamayo@gmail.com>
2023-04-17 11:03:39 -0600

    adding a prefix in index.js for auth endpoints

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-router.post("/api/v1/login", (req, res) => {
+router.post("/login", (req, res) => {
     res.json({login: true});
 });
 
-router.post("/api/v1/register", (req, res) => {
+router.post("/register", (req, res) => {
     res.json({register: true});
 });
 
