bcd60f28baeaf81fcaa7515ee3c04ce55039987a
hftamayo <hftamayo@gmail.com>
2023-04-18 11:17:51 -0600

    validating email input in register endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,17 @@
-export const login = (req, res) => {
-    console.log(req.body);
-    res.json({login: true});
-};
+import { validationResult } from "express-validator";
 
 export const register = (req, res) => {
-    res.json({register: true});
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  console.log(req.body);
+  res.json({ register: true});
+};
+
+export const login = (req, res) => {
+  console.log(req.body);
+  res.json({ login: true });
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import {login, register} from "../controllers/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
+import { body } from "express-validator";
 const router = express.Router();
 
 router.post("/login", login);
 
-router.post("/register", register);
+router.post(
+  "/register",
+  [body("email", "Invalid email format").trim().isEmail().normalizeEmail()],
+  register
+);
 
 export default router;
