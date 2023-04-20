d03af8594b73c73bef49be28bf561d312e97aab2
hftamayo <hftamayo@gmail.com>
2023-04-20 11:21:39 -0600

    refactoring auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,4 @@
-import { validationResult } from "express-validator";
-
 export const register = (req, res) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  console.log(req.body);
   res.json({ register: true});
 };
 
