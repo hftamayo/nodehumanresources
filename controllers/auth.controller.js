a88340e84064a178ffca90cb1e0bf2e6617eab3c
hftamayo <hftamayo@gmail.com>
2023-04-26 17:48:43 -0600

    calculating password hashed part2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,8 @@
-export const register = (req, res) => {
+export const register = async(req, res) => {
   console.log(req.body);
   res.json({ register: true});
 };
 
-export const login = (req, res) => {
+export const login = async(req, res) => {
   res.json({ login: true });
 };
--- models/User.js
+++ models/User.js
@@ -18,12 +18,16 @@ const userSchema = new Schema({
 
 userSchema.pre("save", async function (next) {
   const user = this;
+  
+  if(!user.isModified('password')) return next()
+
   try {
-    const salt = await bcryptjs.hash(10);
-    const hashPassword = await bcryptjs.hash(user.password, salt);
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(user.password, salt);
     next();
   } catch (error) {
     console.log(error);
+    throw new Error("Error generating password hash");
   }
 });
 
