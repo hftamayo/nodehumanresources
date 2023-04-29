2554d02c4d416bae538708ccc24ba5fc820e29fb
hftamayo <hftamayo@gmail.com>
2023-04-29 11:30:04 -0600

    fixing a typo

--- middlewares/validationResultExpress.js
+++ middlewares/validationResultExpress.js
@@ -1,7 +1,7 @@
 import { validationResult } from "express-validator";
 
 export const validationResultExpress = (req, res, next) => {
-  const errors = validationResults(req);
+  const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
