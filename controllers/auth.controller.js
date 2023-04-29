281fa5ffffc58c2e5ba9d7717918f57dd975f7b9
hftamayo <hftamayo@gmail.com>
2023-04-29 12:39:03 -0600

    another approach for user validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,14 +3,17 @@ import { User } from "../models/User.js";
 export const register = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = new User({ email, password });
+    let user = await User.findOne({ email });
+    if (user) throw { code: 11000 };
+
+    user = new User({ email, password });
     await user.save();
     //jwt token
     return res.json({ register: true });
   } catch (error) {
     console.log(error.code);
     if (error.code === 11000) {
-      return res.status(400).json({error: "User already exists"});
+      return res.status(400).json({ error: "User already exists" });
     }
   }
 };
