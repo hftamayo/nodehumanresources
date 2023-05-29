a756366444e28776e4efdce1995e60b115b925f6
hftamayo <hftamayo@gmail.com>
2023-05-29 15:27:37 -0600

    bug during token generation

--- middlewares/requiredToken.js
+++ middlewares/requiredToken.js
@@ -1,9 +1,14 @@
+import jwt from "jsonwebtoken";
+
 export const requiredToken = (req, res, next) => {
   try {
-    const token = req.headers?.authorization;
+    let token = req.headers?.authorization;
     if (!token) {
       throw new Error("Error in token access (Bearer blocker)");
     }
+    token = token.split(" ")[1];
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(payload);
     next();
   } catch (error) {
     console.log(error);
