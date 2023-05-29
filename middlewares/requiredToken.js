e9e5e2ad20c2d3162a20fe6750a62bb57487485f
hftamayo <hftamayo@gmail.com>
2023-05-29 15:02:05 -0600

    evaluating if the header is using bearer

--- middlewares/requiredToken.js
+++ middlewares/requiredToken.js
@@ -1,9 +1,10 @@
 export const requiredToken = (req, res, next) => {
   try {
-    console.log(req.headers);
+    const token = req.headers?.authorization;
+    if (!token) throw new Error("Error in token access (Bearer blocker)");
     next();
-
   } catch (error) {
     console.log(error);
+    return res.status(401).json({ error: error.message });
   }
 };
